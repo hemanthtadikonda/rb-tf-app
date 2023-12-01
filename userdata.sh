2b5342cb02e1edc9bb925e410b6ff56a4b448ad0
hemanth <hemanthtadikonda29@gmail.com>
2023-12-01 14:45:26 +0530

    pc - "apps"

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y &>> /opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/rb-ansible.git main.yml -e component=${component} -e env=${env} /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/rb-ansible.git main.yml -e component=${component} -e env=${env} &>> /opt/userdata.log
