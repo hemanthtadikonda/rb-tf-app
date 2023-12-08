ab9a36e3aaeb2db1241d620d9250c1d222d70eb2
hemanth <hemanthtadikonda29@gmail.com>
2023-12-08 09:52:00 +0530

    pc - "apps"

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "main" {
     from_port        = 9100
     to_port          = 9100
     protocol         = "tcp"
-    cidr_blocks      = [element(module.prometheus["prometheus_ip"], 0)]
+    cidr_blocks      = [element(module.prometheus["prometheus_ip"], count.index)]
   }
   egress {
     from_port        = 0
