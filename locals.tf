95d32b558530baf2bf58f81957a7586a830d6ff8
hemanth <hemanthtadikonda29@gmail.com>
2023-12-08 10:08:07 +0530

    pc - "apps"

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   name_prefix = "${var.env}-${var.component}"
   tags        = merge(var.tags,{ tf-module = "app" },{ env = var.env })
-  ip          = element(module.prometheus , 0 )
+  ip          = tolist(module.prometheus)
 
 }
 
