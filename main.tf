56559193fe496bb9464fdb7b27a3c8e2186ef1a6
hemanth <hemanthtadikonda29@gmail.com>
2023-12-08 09:39:27 +0530

    pc - "apps"

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "main" {
     protocol         = "tcp"
     cidr_blocks      = var.ssh_ingress_cidr
   }
+  ingress {
+    description      = "PROMETHEUS"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = ["${module.prometheus}/32"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
@@ -92,102 +99,102 @@ resource "aws_lb_listener_rule" "public" {
     }
   }
 }
-
-resource "aws_iam_policy" "main" {
-  name        = "${local.name_prefix}-policy"
-  path        = "/"
-  description = "${local.name_prefix}-policy"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:043254050286:parameter/${var.env}.*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "main" {
-  name = "${local.name_prefix}-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = merge( local.tags , { Name = "${local.name_prefix}-role"} )
-
-}
-
-resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.main.name
-  policy_arn = aws_iam_policy.main.arn
-}
-
-resource "aws_iam_instance_profile" "main" {
-  name = "${local.name_prefix}-role-profile"
-  role = aws_iam_role.main.name
-}
-
-
-resource "aws_launch_template" "main" {
-  name = "${local.name_prefix}-template"
-  image_id = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
-  iam_instance_profile {
-    name = "${local.name_prefix}-role-profile"
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = merge(local.tags , {Name = "${local.name_prefix}-tem"})
-  }
-
-  user_data = base64encode(templatefile("${path.module}/userdata.sh" , { component=var.component , env=var.env}))
-}
-
-resource "aws_autoscaling_group" "main" {
-  name_prefix          = "${local.name_prefix}-asg"
-  vpc_zone_identifier = var.app_subnet_ids
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
-  target_group_arns  = [aws_lb_target_group.main.arn]
-
-  launch_template {
-    id      = aws_launch_template.main.id
-    version = "$Latest"
-  }
-  tag {
-    key = "Name"
-    value = local.name_prefix
-    propagate_at_launch = true
-  }
-}
+#
+#resource "aws_iam_policy" "main" {
+#  name        = "${local.name_prefix}-policy"
+#  path        = "/"
+#  description = "${local.name_prefix}-policy"
+#
+#  policy = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "VisualEditor0",
+#        "Effect": "Allow",
+#        "Action": [
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameters",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource": "arn:aws:ssm:us-east-1:043254050286:parameter/${var.env}.*"
+#      },
+#      {
+#        "Sid": "VisualEditor1",
+#        "Effect": "Allow",
+#        "Action": "ssm:DescribeParameters",
+#        "Resource": "*"
+#      }
+#    ]
+#  })
+#}
+#
+#resource "aws_iam_role" "main" {
+#  name = "${local.name_prefix}-role"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole"
+#        Effect = "Allow"
+#        Sid    = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#
+#  tags = merge( local.tags , { Name = "${local.name_prefix}-role"} )
+#
+#}
+#
+#resource "aws_iam_role_policy_attachment" "attach" {
+#  role       = aws_iam_role.main.name
+#  policy_arn = aws_iam_policy.main.arn
+#}
+#
+#resource "aws_iam_instance_profile" "main" {
+#  name = "${local.name_prefix}-role-profile"
+#  role = aws_iam_role.main.name
+#}
+#
+#
+#resource "aws_launch_template" "main" {
+#  name = "${local.name_prefix}-template"
+#  image_id = data.aws_ami.ami.id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [aws_security_group.main.id]
+#  iam_instance_profile {
+#    name = "${local.name_prefix}-role-profile"
+#  }
+#
+#  tag_specifications {
+#    resource_type = "instance"
+#    tags = merge(local.tags , {Name = "${local.name_prefix}-tem"})
+#  }
+#
+#  user_data = base64encode(templatefile("${path.module}/userdata.sh" , { component=var.component , env=var.env}))
+#}
+#
+#resource "aws_autoscaling_group" "main" {
+#  name_prefix          = "${local.name_prefix}-asg"
+#  vpc_zone_identifier = var.app_subnet_ids
+#  desired_capacity   = var.desired_capacity
+#  max_size           = var.max_size
+#  min_size           = var.min_size
+#  target_group_arns  = [aws_lb_target_group.main.arn]
+#
+#  launch_template {
+#    id      = aws_launch_template.main.id
+#    version = "$Latest"
+#  }
+#  tag {
+#    key = "Name"
+#    value = local.name_prefix
+#    propagate_at_launch = true
+#  }
+#}
 
 module "prometheus" {
   source = "./prometheus"
