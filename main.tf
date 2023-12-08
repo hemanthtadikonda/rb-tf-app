555bd5ed69d50830d736a801a345f582ef969c5e
hemanth <hemanthtadikonda29@gmail.com>
2023-12-08 18:44:10 +0530

    pc - "apps"

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "aws_security_group" "main" {
     cidr_blocks      = var.ssh_ingress_cidr
   }
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
+    description      = "PROMETHEUS"
+    from_port        = 9100
+    to_port          = 9100
     protocol         = "tcp"
     cidr_blocks      = local.ip
   }
